8ef3b1dd937dd98def926abac588573c9f676cd4
Taunk714 <taunk.714@gmail.com>
2020-11-17 15:42:19 -0500

    skip some position in ab alg

--- pa2.py
+++ pa2.py
@@ -14,7 +14,6 @@
 from pa2_gomoku import Player
 
 
-
 class AIPlayer(Player):
     """ a subclass of Player that looks ahead some number of moves and 
     strategically determines its best next move.
@@ -37,31 +36,31 @@ class AIPlayer(Player):
             return: row, col are the coordinated of a vacant location on the board 
         """
         self.num_moves += 1
-        
-        
+
         ################### TODO: ######################################
         # Implement your strategy here. 
         # Feel free to call as many as helper functions as you want.
         # We only cares the return of this function
         ################################################################
 
-                # Here I just try to find the position that has max score.
+        # Here I just try to find the position that has max score.
         # But I am not sure whether it is a proper way.
-        #
-        # I have another idea that we make minimizer and maximizer to be mirror.
-        # (When maximizer and minimizer face the same situation, maximizer's score is k, minimizer's score is -k)
-        # then when we get the score for every position, we compare the biggest score
-        # and smallest score, choose the position who has bigger abs(score).
-        # (Because if minimizer has a extremely small number, and we don't block it,
-        # probably the minimizer will win)
+        # FEEL FREE TO CHANGE ANYTHING!
 
-        max_score = -1 # keep the max score
-        best_row, best_col = 0, 0 # keep the position of best score
-        alpha = -1
+        max_score = -10000  # keep the max score
+        best_row, best_col = 0, 0  # keep the position of best score
+        alpha = -10000
         beta = 10000
 
+        # first step of alpha beta alg.
         for row in range(len(board)):
             for col in range(len(board)):
+                # skip some positions(see alphabeta in detail)
+                if not board.can_add_to(row, col):
+                    continue
+                if self.is_isolated(row, col, board):
+                    continue
+
                 score = self.alphabeta(board, row, col, self.depth, alpha, beta, False)
                 if score > max_score:
                     max_score = score
@@ -77,34 +76,59 @@ class AIPlayer(Player):
         """
         # Need a function that calculate score based on the current board
         # Need a function to check whether it is a terminal node(game over)
+        # We can use the same function to calculate both minimizer and maximizer,
+        # But maximizer should be f(args), minimizer should be -f(args).
         if depth == 0 or "node is a terminal node":
             return "the heuristic value of node"
         if maximizingPlayer:
             value = -100000
             for i in range(len(board)):
                 for j in range(len(board)):
-                    # This is the easiest way, but cost too much time. We only need to
-                    # consider positions that are close to the checkers that already in the board.
-                    # Then after pruning, we can go deeper.
-                    if board.can_add_to(i, j):
-                        # It is wrong here. In this way(use board as input argument),
-                        # we need a function that can remove checker from the board. This should be a
-                        # backtracking algorithm, we should add the check and then remove.
-                        value = max(value, self.alphabeta(board, i, j, depth - 1, alpha, beta, False))
-                        alpha = max(alpha, value)
-                        if alpha >= beta:
-                            break  # (* beta cutoff *)
+                    # If this position if full, skip
+                    if not board.can_add_to(i, j):
+                        continue
+
+                    # if this position is isolated, (no other checkers nearby), we skip
+                    # which position can be isolated? 2-layer or 1 layer? 2-layer means:
+                    # _____
+                    # _____
+                    # __X__
+                    # _____
+                    # _____
+                    # And we can keep the situation in an array like board, so we do not to compute repeatedly.
+                    if self.is_isolated(i, j, board):
+                        continue
+
+                    # It is wrong here. In this way(use board as input argument),
+                    # we need a function that can remove checker from the board. This should be a
+                    # backtracking algorithm, we should add the check and then remove.
+                    value = max(value, self.alphabeta(board, i, j, depth - 1, alpha, beta, False))
+                    alpha = max(alpha, value)
+                    if alpha >= beta:
+                        break  # (* beta cutoff *)
             return value
         else:
             value = 100000
             for i in range(len(board)):
                 for j in range(len(board)):
+                    if not board.can_add_to(i, j):
+                        continue
+
+                    if self.is_isolated(i, j, board):
+                        continue
+
                     if board.can_add_to(i, j):
                         value = min(value, self, self.alphabeta(board, i, j, depth - 1, alpha, beta, True))
                         beta = min(beta, value)
                         if beta <= alpha:
                             break  # (* alpha cutoff *)
             return value
+
+    def is_isolated(self, row, col, borad):
+        return True
+
+    def compute_score(self, row, col, board):
+        return
 
     # def meet_tree(self, board, choice):
     #
